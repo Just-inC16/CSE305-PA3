f24c3e2c2a72beb1ec06df45275a3f4cd795d790
Just-inC16 <justin.chan.1@stonybrook.edu>
2021-11-30 07:40:30 -0500

    Fixed the auction SQL

--- src/SQL/auction.sql
+++ src/SQL/auction.sql
@@ -26,11 +26,11 @@ CREATE TABLE Auction (
 
 INSERT INTO Auction(auctionID, itemID,employeeID,bidIncrement, minimumBid, copiesSold, monitor,  closingBid,currentBid,currentHighBid,reserve)
 VALUES
-	(1, 123, "juschan",2.0, 5.50, 15, 50, 200000000,30,25,26),
-    (2,456,"danjlee", 1.0, 14620.00, 20000, 100, 200000000,31,25,26);
+	(1, 123, "jusch",2.0, 5.50, 15, 50, 200000000,30,25,26),
+    (2,456,"ben10", 1.0, 14620.00, 20000, 100, 200000000,31,25,26);
 -- (3, 1,"junholee",1.0, 900.00, 1500, 25, 300000000, 32,25,26);
 --     (4, 2,1,1.0, 1800.00, 3000, 25, 300000000,33,25,26,24),
 -- 	(5, 1,1,1.0, 1400.00, 2000, 25, 300000000,34,25,26,24),
 --     (6, 2,1,2.0, 5.50, 15, 50, 200000000,35,25,26,24);
     
-select * from Auction;
+select * from Auction, employee,item;
