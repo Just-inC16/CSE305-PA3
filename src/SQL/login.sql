455541663db991e15265095d190b9193d4f9eefd
Just-inC16 <justin.chan.1@stonybrook.edu>
2021-12-07 12:57:43 -0500

    Changing the password

--- src/SQL/login.sql
+++ src/SQL/login.sql
@@ -23,4 +23,17 @@ AND Login.password_ = "jc123";
 
 
 select * from Login;
-INSERT INTO Employee(employeeID, startDate, hourlyRate, level_, firstName, lastName, address, city, state, zipCode, email, telephone, revenue) VALUES ('12332131', 'jdslfj', '78.0', 'null', 'ChongABC', 'jgl', 'lkdf', 'gjlkewj', 'NJ', 122321, 'dsfdsf@d', '123123', 'null', );
+
+-- Change the login credentials for the users 
+Update Login 
+SET username='dl@g', password_='456'
+WHERE username='dl@gmail.com';
+
+Update Login 
+SET username='jc@g', password_='123'
+WHERE username='jc@gmail.com';
+
+Update Login 
+SET username='dl@g', password_='456'
+WHERE username='dl@gmail.com';
+-- INSERT INTO Employee(employeeID, startDate, hourlyRate, level_, firstName, lastName, address, city, state, zipCode, email, telephone, revenue) VALUES ('12332131', 'jdslfj', '78.0', 'null', 'ChongABC', 'jgl', 'lkdf', 'gjlkewj', 'NJ', 122321, 'dsfdsf@d', '123123', 'null', );
